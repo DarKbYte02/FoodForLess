86aaa0c8f28a815056d9b88d102bb0a704ca90dc
DarKbYte02 <danielromero6665@gmail.com>
2024-11-17 23:17:08 -0600

    Creation of repository

---
+++ src/main/java/com/ipn/mx/foodforless/FoodForLessApplication.java
@@ -0,0 +1,13 @@
+package com.ipn.mx.foodforless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodForLessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodForLessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipn/mx/foodforless/FoodForLessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipn.mx.foodforless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodForLessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
