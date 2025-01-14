9182a347d0796dd39018de684c0288445a015064
Juan Antonio FA <86801514+Juan-Antonio-FA@users.noreply.github.com>
2025-01-13 23:58:43 -0600

    ObtenerPedidos

--- frontend/src/app/orders/orders/orders.component.ts
+++ frontend/src/app/orders/orders/orders.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+import {PedidoService} from "../../services/pedido.service";
+import {Pedido} from "../../pedido";
+import {FooterComponent} from "../../shared/footer/footer.component";
+import {HeaderComponent} from "../../shared/header/header.component";
+import {CommonModule} from '@angular/common';
+import {RouterModule, Router} from '@angular/router';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [
+    FooterComponent,
+    HeaderComponent,
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.css'
+  styleUrl: './orders.component.css',
+  providers:[CookieService]
 })
 export class OrdersComponent {
+    private cookieService = inject(CookieService);
+    private pedidoService = inject(PedidoService);
+    private router = inject(Router);
+    userId:string = this.cookieService.get('userId');
+    pedidos: Pedido[] = [];
+
+    constructor(){
+        this.pedidoService.list().subscribe((data: any) => {
+        for(let i = 0; i < data.length; i++){
+            if(data[i].user.idUser == this.userId){
+            this.pedidos.push(data[i]);
+            }
+        }
+        console.log(this.pedidos);
+        });
+    }
+
+    deleteOrder(id:any){
+        this.pedidoService.delete(id).subscribe((response: any) => {
+        alert('Pedido eliminado');
+        window.location.reload();
+        this.router.navigate(['orders']);
+        });
+    }
 
 }
